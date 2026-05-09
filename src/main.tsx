75c67b8f90f59213b0db3b9f075e30484a09c529
Mostafa Eldaly <dalyxiv@gmail.com>
2026-05-10 01:48:56 +0200

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from '@tanstack/react-router'
+import { getRouter } from './router'
+import './styles.css' // This ensures your styles load
+
+const router = getRouter()
+
+// This connects your code to the <div id="root"> in your index.html
+const rootElement = document.getElementById('root')!
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  )
+}
